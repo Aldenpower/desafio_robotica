92213cd728ace2ac2855c951f54e593b526fcb9c
Aldenpower <anderson_afsl@hotmail.com>
2021-03-05 17:56:39 -0300

    First try : avoiding obstacles

--- webots_project/controllers/sensors_obstacles/sensors_obstacles.py
+++ webots_project/controllers/sensors_obstacles/sensors_obstacles.py
@@ -35,9 +35,30 @@ rightMotor.setPosition(float('inf'))
 leftMotor.setVelocity(0.0)
 rightMotor.setVelocity(0.0)
 
-# Laser callback
-def laser_callback(laser_list):
-    pass
+# Sonar laser regions callback
+def s_c(laser_list, tag = '', pr = False):
+    lasers_range = {
+    'right': max(laser_list[7], laser_list[8]),
+    'left': max(laser_list[0], laser_list[15]),
+    'fright': max(laser_list[5], laser_list[6]),
+    'front': max(laser_list[3], laser_list[4]),
+    'fleft': max(laser_list[1], laser_list[2]),
+    }
+
+    if tag == 'right':
+        return lasers_range['right']
+    if tag == 'left':
+        return lasers_range['left']
+    if tag == 'fright':
+        return lasers_range['fright']
+    if tag == 'front':
+        return lasers_range['front']
+    if tag == 'fleft':
+        return lasers_range['fleft']
+    
+    if pr:
+        print(lasers_range)
+        return lasers_range
 
 #RUN
 
@@ -45,12 +66,62 @@ while robot.step(TIME_STEP) != -1:
     # Read the sensors:
     dsValues = []
     for i in range(MAX_SENSOR_NUMBER):
-        dsValues.append(ds_[i].getValue())
+        dsValues.append(round(ds_[i].getValue(), 1))
     
+    if s_c(dsValues, 'front') < 950 and s_c(dsValues, 'fleft') < 950 and s_c(dsValues, 'fright') < 950:
+        # Move straight
+        leftSpeed  = 0.5 * MAX_SPEED
+        rightSpeed = 0.5 * MAX_SPEED
+
+    elif s_c(dsValues, 'front') > 950 and s_c(dsValues, 'fleft') < 950 and s_c(dsValues, 'fright') < 950:
+        # Front obstacle YES Left NO Right NO
+        leftSpeed  = 0.5 * MAX_SPEED
+        rightSpeed = -0.5 * MAX_SPEED
+    
+    elif s_c(dsValues, 'front') < 950 and s_c(dsValues, 'fleft') < 950 and s_c(dsValues, 'fright') > 950:
+        # Front obstacle NO left NO Right YES
+        leftSpeed  = -0.5 * MAX_SPEED
+        rightSpeed = 0.5 * MAX_SPEED
+    
+    elif s_c(dsValues, 'front') < 950 and s_c(dsValues, 'fleft') > 950 and s_c(dsValues, 'fright') < 950:
+        # Front obstacle NO left YES Right NO
+        leftSpeed  = 0.5 * MAX_SPEED
+        rightSpeed = -0.5 * MAX_SPEED
+    
+    elif s_c(dsValues, 'front') > 950 and s_c(dsValues, 'fleft') < 950 and s_c(dsValues, 'fright') > 950:
+        # Front obstacle YES left NO Right YES
+        leftSpeed  = -0.5 * MAX_SPEED
+        rightSpeed = 0.5 * MAX_SPEED
+    
+    elif s_c(dsValues, 'front') > 950 and s_c(dsValues, 'fleft') > 950 and s_c(dsValues, 'fright') < 950:
+        # Front obstacle YES left YES Right NO
+        leftSpeed  = 0.5 * MAX_SPEED
+        rightSpeed = -0.5 * MAX_SPEED
+
+    elif s_c(dsValues, 'front') > 950 and s_c(dsValues, 'fleft') > 950 and s_c(dsValues, 'fright') > 950:
+        # Front obstacle YES left YES Right YES
+        leftSpeed  = -0.5 * MAX_SPEED
+        rightSpeed = -0.5 * MAX_SPEED
+    
+    elif s_c(dsValues, 'front') < 950 and s_c(dsValues, 'fleft') > 950 and s_c(dsValues, 'fright') > 950:
+        # Front obstacle NO left YES Right YES
+        leftSpeed  = -0.5 * MAX_SPEED
+        rightSpeed = -0.5 * MAX_SPEED
+    
+    else:
+        print('Unknown case')
+    
+
+    
+
+
+    
+    leftMotor.setVelocity(leftSpeed)
+    rightMotor.setVelocity(rightSpeed)
     print(dsValues)
-    
-    laser_callback(dsValues)
-     
+    s_c(dsValues, pr = True)
+
+
     # detect obstacles
     #right_obstacle = dsValues[15] < 900 or dsValues[0] < 900
     #left_obstacle = dsValues[8] < 900 or dsValues[7] < 900
